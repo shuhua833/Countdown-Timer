c68666392e1579332093ccb5d889b4ca3a7686be
Shuhua Shrestha <71085603+shuhua833@users.noreply.github.com>
2022-11-26 20:30:06 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-
-// The clock should take time in seconds and should do a countdown until it hit's zero and when it hits zero I want to see a generic message like Timer Expired or anything
-// I want to see the countdown logic implemented inside it's own customHook
-
 import './App.css';
 import {   useEffect, useState} from "react";
-import useFetch from "./hooks/useFetch";
+
+import useCounter from './hooks/useCounter';
 function App(){
-  const [inputValue, setInputValue] = useFetch();
+  const [inputValue, setInputValue] = useCounter();
 
   return (
     <>
@@ -15,9 +12,9 @@ function App(){
         type="text"
         value={inputValue}
        onChange={(e)=>setInputValue(e.target.value)} 
-      />
-      
+      />  
       <div>{inputValue}</div>
+      {inputValue===0?<div>Timer expired</div>:<></>}
     </>
   );
 }
